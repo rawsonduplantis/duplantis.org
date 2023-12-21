aee40672077f23909c6e59b4bf854568342ad43e
Rawson Duplantis <46237409+ishkena@users.noreply.github.com>
2023-12-20 20:01:43 -0600

    unused errors resolved

--- src/routes/dev.tsx
+++ src/routes/dev.tsx
@@ -1,25 +1,4 @@
-import Markdown from "react-markdown"
-import Post from "../components/dev/Post"
-import { render } from "react-dom"
-import { Outlet } from "react-router-dom"
 import PostLink from "../components/dev/PostLink"
-
-{/* <body className="bg-orange-100 h-full w-screen">
-            <div className="flex flex-col">
-                <div className="m-auto w-1/2">
-                    <div className="m-4">
-                        <p className="text-1xl text-orange-950/75 text-center">DevBlog</p>
-                        <p className="text-1xl text-orange-950/75 text-center">More to come...</p>
-                    </div>
-                    
-                    <div className="bg-white p-4">
-                        <Markdown className={"prose"}>
-                            {markdown}
-                        </Markdown>
-                    </div>
-                </div>
-            </div>
-        </body> */}
 
 export default function Dev() {
 
