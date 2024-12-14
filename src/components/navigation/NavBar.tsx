bb5d4ec6340e60ac496978202b438328a298cc0c
ishkena <rduplantis@live.com>
2024-12-14 02:15:20 -0600

    fixed navbar display bug

--- src/components/navigation/NavBar.tsx
+++ src/components/navigation/NavBar.tsx
@@ -19,7 +19,7 @@ export default function NavBar() {
                 <NavLink destination="about" home={false} />
                 {/*<NavLink destination="imperialism" home={false} />*/}
             </div>
-            <div className="flex flex-row h-16 bg-orange-100 sm:inline md:hidden">
+            <div className="flex flex-row h-16 bg-orange-100 md:hidden">
                 <NavLink destination="duplantis.org" home={true}/>
                 <button onClick={dropDown} className="ml-auto pr-5 self-center">
                     <svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="#724B3B"><path d="M120-240v-66.67h720V-240H120Zm0-206.67v-66.66h720v66.66H120Zm0-206.66V-720h720v66.67H120Z"/></svg>
