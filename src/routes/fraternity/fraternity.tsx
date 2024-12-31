eee01ea95351f24d731886d977f9852d8737ec1d
rawsonduplantis <rawson@duplantis.org>
2024-12-31 01:26:06 -0600

    styled fraternity login page

--- src/routes/fraternity/fraternity.tsx
+++ src/routes/fraternity/fraternity.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
+import NavBar from "../../components/navigation/NavBar"
+import { log } from "console"
 
 export default function Fraternity() {
     const nav = useNavigate()
@@ -30,14 +32,23 @@ export default function Fraternity() {
         }
     }, [])
 
-    /*
+    
     function logout() {
         setToken('')
         localStorage.removeItem('jwt-token')
+        nav('/fraternity/login')
     }
-    */
+    
 
     return(<>
-        <p>{userData?.safehouseKey}</p>
+        <NavBar />
+        <body className="bg-orange-100/75 h-full w-screen pt-16">
+            <div className="flex h-full">
+                <div className="m-auto">
+                    <p className="text-1xl text-orange-950/75 text-center cursor-default select-none">{userData?.safehouseKey}</p>
+                    <button className="w-24 ml-auto mr-auto bg-orange-950/75 hover:bg-orange-950/80 active:bg-orange-950/90 text-white py-2 px-4 rounded-lg font-semibold m-auto" onClick={logout}>Logout</button>
+                </div>
+            </div>
+        </body>
     </>)
 }
