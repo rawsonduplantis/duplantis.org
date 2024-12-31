a214afec384f87cefaac1a704cb986c4b038fb35
rawsonduplantis <rawson@duplantis.org>
2024-12-31 05:20:50 -0600

    fitted tree properly and added title

--- src/routes/fulltree.tsx
+++ src/routes/fulltree.tsx
@@ -1,9 +1,18 @@
 import { ReactComponent as FamilyTreeGen3 } from '../unused components/tree/trees/familytreegen3.svg';
+import { useEffect, useRef } from 'react';
 
 export default function FullTree() {
+    const svgRef = useRef<SVGSVGElement | null>(null)
+    
+    useEffect(() => {
+        const svg = svgRef.current
+        if (svg) {
+            const bbox = svg.getBBox()
+            svg.setAttribute('viewBox', `${bbox.x} ${bbox.y} ${bbox.width} ${bbox.height}`)
+        }
+    }, [])
+
     return(<>
-        <div className='p-16'>
-            <FamilyTreeGen3 />  
-        </div>
+        <FamilyTreeGen3 />  
     </>)
 }
