c921fed1cef05ef3d979b84b805567cfaab6ce8e
rawsonduplantis <rawson@duplantis.org>
2024-12-31 03:02:12 -0600

    Styled logout and session display

--- src/routes/fraternity/fraternity.tsx
+++ src/routes/fraternity/fraternity.tsx
@@ -1,14 +1,16 @@
 import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import NavBar from "../../components/navigation/NavBar"
-import { log } from "console"
 
 export default function Fraternity() {
     const nav = useNavigate()
     const [token, setToken] = useState<string>('')
     interface UserData {
         safehouseKey: string;
-        // add other properties as needed
+        decodedToken: {
+            scroll: string;
+            lastname: string;
+        }
     }
 
     const [userData, setUserData] = useState<UserData | null>(null)
@@ -46,11 +48,14 @@ export default function Fraternity() {
     
     return (<>
         <NavBar />
+        <div className="absolute right-0 mr-4 flex flex-row h-16">
+            <p className="m-auto mr-4 text-1xl text-orange-950/75 font-semibold">{`${userData?.decodedToken.lastname || 'NOT LOGGED IN'} #${userData?.decodedToken.scroll || 'NO SCROLL FOUND'}`}</p>
+            <button className="w-24 ml-auto mr-auto bg-orange-950/75 hover:bg-orange-950/80 active:bg-orange-950/90 text-white py-2 px-4 rounded-lg font-semibold m-auto" onClick={logout}>Logout</button>
+        </div>
         <div className="bg-orange-100/75 h-full w-screen pt-16">
             <div className="flex h-full">
                 <div className="m-auto">
-                    <p className="text-1xl text-orange-950/75 text-center cursor-default select-none">{userData?.safehouseKey}</p>
-                    <button className="w-24 ml-auto mr-auto bg-orange-950/75 hover:bg-orange-950/80 active:bg-orange-950/90 text-white py-2 px-4 rounded-lg font-semibold m-auto" onClick={logout}>Logout</button>
+                    <p className="text-1xl text-orange-950/75 text-center cursor-default select-none">{userData?.decodedToken.scroll || 'loading'}</p>
                 </div>
             </div>
         </div>
