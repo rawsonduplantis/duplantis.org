c0f4703eee9e00de73625d4356a1f7e5e55d4a0c
rawsonduplantis <rawson@duplantis.org>
2025-01-01 16:49:32 -0600

    made login mobile responsive

--- src/routes/fraternity/fraternitylogin.tsx
+++ src/routes/fraternity/fraternitylogin.tsx
@@ -38,21 +38,26 @@ export default function FraternityLogin() {
             <div className="flex h-full">
                 <div className="m-auto w-4/5 md:w-1/2 xl:w-1/3 2xl:w-1/4">                    
                     <form onSubmit={submitUser} className="space-y-4 bg-orange-50/75 border border-orange-950/75 p-4 rounded-lg flex-col w-full">
-                        <div className="flex items-center justify-center w-full">
-                            <input id="lastname" type="text" name="lastname" placeholder="Last Name" onChange={(e) => setLastname(e.target.value)}
-                                className="border-b border-orange-950/75 bg-transparent focus:ring-2 focus:ring-blue-500 focus:outline-none w-24 text-orange-950/75 placeholder:text-orange-950/25 text-center" required />
-                            <p className='text-1xl text-orange-950/75'>,&nbsp;#</p>
-                            <input id="scroll" type="text" name="scroll" onChange={(e) => setScroll(e.target.value)}
-                                className="border-b border-orange-950/75 bg-transparent focus:ring-2 focus:ring-blue-500 focus:outline-none w-7 text-center text-orange-950/75" required />
-                            <p className='text-1xl text-orange-950/75'>,&nbsp;and&nbsp;I&nbsp;</p>
-                            <input id="codestart" type="password" name="codestart" onChange={(e) => setCodestart(e.target.value)}
-                                className="border-b border-orange-950/75 bg-transparent focus:ring-2 focus:ring-blue-500 focus:outline-none w-10 text-orange-950/75" required />
-                            <p className='text-1xl text-orange-950/75'>&nbsp;this&nbsp;</p>
-                            <input id="codeend" type="password" name="codeend" onChange={(e) => setCodeend(e.target.value)}
-                                className="border-b border-orange-950/75 bg-transparent focus:ring-2 focus:ring-blue-500 focus:outline-none w-24 text-orange-950/75" required />
-                            <p className='text-1xl text-orange-950/75'>.</p>
+                        <div className="md:flex md:items-center justify-center w-full">
+                            <div className='flex items-center justify-center'>
+                                <input id="lastname" type="text" name="lastname" placeholder="Last Name" onChange={(e) => setLastname(e.target.value)}
+                                    className="border-b border-orange-950/75 bg-transparent focus:outline-none w-24 text-orange-950/75 placeholder:text-orange-950/25 text-center" required />
+                                <p className='text-1xl text-orange-950/75'>,&nbsp;#</p>
+                                <input id="scroll" type="text" name="scroll" onChange={(e) => setScroll(e.target.value)}
+                                    className="border-b border-orange-950/75 bg-transparent focus:outline-none w-7 text-center text-orange-950/75" required />
+                                    <p className='text-1xl text-orange-950/75'>,&nbsp;and&nbsp;</p>
+                            </div>
+                            <div className='flex items-center justify-center'>
+                                <p className='text-1xl text-orange-950/75'>I&nbsp;</p>
+                                <input id="codestart" type="password" name="codestart" onChange={(e) => setCodestart(e.target.value)}
+                                    className="border-b border-orange-950/75 bg-transparent focus:outline-none w-10 text-orange-950/75" required />
+                                <p className='text-1xl text-orange-950/75'>&nbsp;this&nbsp;</p>
+                                <input id="codeend" type="password" name="codeend" onChange={(e) => setCodeend(e.target.value)}
+                                    className="border-b border-orange-950/75 bg-transparent focus:outline-none w-24 text-orange-950/75" required />
+                                <p className='text-1xl text-orange-950/75'>.</p>
+                            </div>
                         </div>
-                        <button type="submit" className="w-full ml-auto mr-auto bg-orange-950/75 hover:bg-orange-950/80 active:bg-orange-950/90 text-white py-2 px-4 rounded-lg font-semibold">Submit</button>
+                        <button type="submit" className="w-full ml-auto mr-auto bg-orange-950/75 hover:bg-orange-950/80 active:bg-orange-950/90 text-white py-2 px-4 rounded-lg font-semibold focus:outline-none focus:ring focus:ring-orange-950/25">Enter</button>
                     </form>
                 </div>
             </div>
